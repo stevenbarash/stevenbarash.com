0a342f6d7185ab432f88272295b55863d417f8c9
Steven Barash <stevenbarash6@gmail.com>
2019-12-22 11:36:22 -0800

    init

---
+++ build/precache-manifest.f35b616672fa82bad3522ab7c1490f21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "516f23719348723ea896534d2881ebca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "302d7e1e0ddcc030ce04",
+    "url": "/static/css/main.5952baed.chunk.css"
+  },
+  {
+    "revision": "882fb2bf12ec5e5ab976",
+    "url": "/static/js/2.981f492f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.981f492f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "302d7e1e0ddcc030ce04",
+    "url": "/static/js/main.6909216c.chunk.js"
+  },
+  {
+    "revision": "6aaecce38c07875e3f60",
+    "url": "/static/js/runtime-main.eb59348a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
