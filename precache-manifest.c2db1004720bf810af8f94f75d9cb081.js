cc05da67c5562a72d79945762127e7fb5be3446c
Steven Barash <stevenbarash6@gmail.com>
2020-01-18 17:37:50 -0500

    Updates

---
+++ precache-manifest.c2db1004720bf810af8f94f75d9cb081.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b92d30a1b7e074c9bb4452e5fa8645",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f16d1ed382797d7d70df",
+    "url": "/static/css/main.0a2dd305.chunk.css"
+  },
+  {
+    "revision": "6a194c4a10d5136081c7",
+    "url": "/static/js/2.32da7699.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.32da7699.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f16d1ed382797d7d70df",
+    "url": "/static/js/main.2c9a0195.chunk.js"
+  },
+  {
+    "revision": "6aaecce38c07875e3f60",
+    "url": "/static/js/runtime-main.eb59348a.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
